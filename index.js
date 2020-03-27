61edc766c9f98418ca196329738813c10a19398f
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-27 22:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for(let i = 0; i < array.length; ++i) {
-    if (i == stopValue - 1) {
+    if (array[i] == stopValue) {
       break
     }
     array[i] = changeValue
